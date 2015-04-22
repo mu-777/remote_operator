1aff194f91d16d62512a436be20b68106e36fe72
mu-777 <ryosuke.murata.7@gmail.com>
2015-04-22 21:59:29 +0900

    refactored

--- src/remote_operator.py
+++ src/remote_operator.py
@@ -16,35 +16,35 @@ JOY_TOPIC_NAME = 'joy'
 class RemoteOperator(object):
     def __init__(self):
 
-        self.robot_num = rospy.get_param('~robot_num', ROBOT_NUM)
-        self.pub_remote_commands = rospy.Publisher(REMOTE_COMMAND_TOPIC_NAME, RemoteCommands, queue_size=1)
-        self.remote_commands = RemoteCommands()
+        self._robot_num = rospy.get_param('~robot_num', ROBOT_NUM)
+        self._pub_remote_commands = rospy.Publisher(REMOTE_COMMAND_TOPIC_NAME, RemoteCommands, queue_size=1)
+        self._remote_commands = RemoteCommands()
         self.is_active = False
 
         self._initialize_data()
 
     def _initialize_data(self):
-        for idx in range(self.robot_num):
-            self.remote_commands.list.append(RemoteCommand())
+        for idx in range(self._robot_num):
+            self._remote_commands.list.append(RemoteCommand())
 
     def activate(self):
-        rospy.Subscriber(JOY_TOPIC_NAME, Joy, self.joy_callback)
+        rospy.Subscriber(JOY_TOPIC_NAME, Joy, self._joy_callback)
         self.is_active = True
 
-    def joy_callback(self, joy_data):
+    def _joy_callback(self, joy_data):
         remote_control_flag = False
         v = 0.0
         w = 0.0
-        for idx in range(self.robot_num):
-            self.remote_commands.list[idx].id = idx + 1
-            self.remote_commands.list[idx].is_remote_controlled = remote_control_flag
-            self.remote_commands.list[idx].v = v
-            self.remote_commands.list[idx].w = w
+        for idx in range(self._robot_num):
+            self._remote_commands.list[idx].id = idx + 1
+            self._remote_commands.list[idx].is_remote_controlled = remote_control_flag
+            self._remote_commands.list[idx].v = v
+            self._remote_commands.list[idx].w = w
 
     def publish_data(self):
         if not self.is_active:
             self.activate()
-        self.pub_remote_commands.publish(self.remote_commands)
+        self._pub_remote_commands.publish(self._remote_commands)
 
 
 # --------------------------------------------
